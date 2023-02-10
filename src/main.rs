ad57249191d2aeec2f91d501bd893c4189e8f852
Jeremy Hager <jeremy.hager@disqo.com>
2023-02-10 15:15:23 -0800

    contcatonate via array

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,7 @@
 fn main() {
-    let person_name_slice = "Donlard Mallard";
-    let person_name_string = person_name_slice.to_string();
-    // other options
-    let person_name_string1 = "Donlard Mallard".to_string();
-    let person_name_string2 = String::from("Donald Mallard");
-    // convert string to string slice
-    let person_name_slice_from_string = &person_name_string;
-    let person_name_slice_from_string_pointer = person_name_slice_from_string.as_str();
+    let duck = "Duck";
+    let airlines = "Airlines";
 
+    let airline_name = [duck, " ", airlines].concat();
+    println!("{}", airline_name);
 }
