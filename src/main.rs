c660b6e4e14cb8b0a8a70111949fe23118dc5003
Jeremy Hager <jeremy.hager@disqo.com>
2023-02-10 15:58:20 -0800

    order of operations

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
 fn main() {
-    let squared = i32::pow(8, 2);
-    let floating_integer = f32::powi(6.5, 3);
-    let float_float = f32::powf(6.5, 3.14);
-    println!("integer: {}", squared);
-    println!("float to int: {}", floating_integer);
-    println!("float to float: {}", float_float);
+    let order_ops = 8+4*2-(12/3+7)+4;
+    println!("{}", order_ops);
 }
