00c51b29841390bc051c9dbb5483ef2708bb8dca
Jeremy Hager <jeremy.hager@disqo.com>
2023-02-10 15:57:03 -0800

    using pow

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
 fn main() {
-    let modulus = 18 % 7;
-    println!("{}", modulus);
+    let squared = i32::pow(8, 2);
+    let floating_integer = f32::powi(6.5, 3);
+    let float_float = f32::powf(6.5, 3.14);
+    println!("integer: {}", squared);
+    println!("float to int: {}", floating_integer);
+    println!("float to float: {}", float_float);
 }
