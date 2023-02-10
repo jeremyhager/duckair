75b170e1adb8e1b524ec0364da4e9d0c15d627b0
Jeremy Hager <jeremy.hager@disqo.com>
2023-02-09 16:56:33 -0800

    testing out str and String

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
-    let location = ("KCLE",41.40, -81.85);
-    let (name, latitude, longitude) = location;
-    println!("Location name: {}, latitude: {}, longitude: {}",
-    name, latitude, longitude);
+    let person_name_slice = "Donlard Mallard";
+    let person_name_string = person_name_slice.to_string();
+    // other options
+    let person_name_string1 = "Donlard Mallard".to_string();
+    let person_name_string2 = String::from("Donald Mallard");
+    // convert string to string slice
+    let person_name_slice_from_string = &person_name_string;
+    let person_name_slice_from_string_pointer = person_name_slice_from_string.as_str();
+
 }
