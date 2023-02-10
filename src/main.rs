92b5ae654ea4b3234258eb400bd698066a4db2b0
Jeremy Hager <jeremy.hager@disqo.com>
2023-02-09 16:52:11 -0800

    starting off with arrays and tuples

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let location = ("KCLE",41.40, -81.85);
+    let (name, latitude, longitude) = location;
+    println!("Location name: {}, latitude: {}, longitude: {}",
+    name, latitude, longitude);
+}
