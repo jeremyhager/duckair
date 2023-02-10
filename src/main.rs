1a6ec0530637c4d9d7b41e8554f252a956436837
Jeremy Hager <jeremy.hager@disqo.com>
2023-02-10 15:54:18 -0800

    simple modules example

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
 fn main() {
-    let float_thirty_two: f32 = 17.2;
-    let unsigned_eight: u8 = 5;
-    let cast_unsigned_eight = unsigned_eight as f32;
-
-    let result = float_thirty_two / cast_unsigned_eight;
-    println!("{}", result);
+    let modules = 18 % 7;
+    println!("{}", modules);
 }
