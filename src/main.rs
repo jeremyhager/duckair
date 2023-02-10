c702d85a3e8912341bbdd72e05bd6f7d2424d4e2
Jeremy Hager <jeremy.hager@disqo.com>
2023-02-10 15:41:34 -0800

    wrapped up casting

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
 fn main() {
-    let _warning_var = 0;
+    let float_thirty_two: f32 = 17.2;
+    let unsigned_eight: u8 = 5;
+    let cast_unsigned_eight = unsigned_eight as f32;
+
+    let result = float_thirty_two / cast_unsigned_eight;
+    println!("{}", result);
 }
