a9cc8253a1aca1aff8632d5a5558a197d6301303
Jeremy Hager <jeremy.hager@disqo.com>
2023-02-10 15:20:30 -0800

    using push_str() and push() methods

--- src/main.rs
+++ src/main.rs
@@ -4,4 +4,10 @@ fn main() {
 
     let airline_name = format!("{} {}", duck, airlines);
     println!("{}", airline_name);
+
+    let mut slogan = String::new();
+    slogan.push_str("We hit the ground");
+    slogan.push(' ');
+    slogan = slogan + "every time";
+    println!("{}", slogan);
 }
