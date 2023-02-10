160f49cf9fb5a4f2fec6eb270ddb609331628554
Jeremy Hager <jeremy.hager@disqo.com>
2023-02-10 15:17:20 -0800

    bug fix

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use std::fmt::format;
-
 fn main() {
     let duck = "Duck";
     let airlines = "Airlines";
 
-    let airline_name = format!("{} {}", duck, airliness);
+    let airline_name = format!("{} {}", duck, airlines);
     println!("{}", airline_name);
 }
