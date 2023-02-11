b0e4af1fbc38440841f78c81cb4d29b84e8873ef
Jeremy Hager <jeremy.hager@disqo.com>
2023-02-10 16:01:09 -0800

    true and false ops

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 fn main() {
-    let order_ops = 8+4*2-(12/3+7)+4;
-    println!("{}", order_ops);
+    // let are_equal_is_true = 1 == 1;
+    // let are_equal_is_false = 1 == 2;
+    // let are_not_equal = 1 != 2;
+
+    let is_true = true;
+    let is_false = !is_true;
+
+    println!("is_true: {}, is_false: {}", is_true, is_false);
 }
