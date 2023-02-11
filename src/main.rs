ebf3c1a2d8454948d773f86385748c9d158934da
Jeremy Hager <jeremy.hager@disqo.com>
2023-02-10 16:04:47 -0800

    true, false, and all that

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
+
+    let have_driver_licese = false;
+    let have_passport = false;
+    let have_proof = have_driver_licese || have_passport;
+
     let have_boarding_pass = true;
-    let have_id = false;
+    let have_id = have_proof;
     let can_board  = have_boarding_pass && have_id;
 
     println!("Boarding Pass: {}, ID: {}", have_boarding_pass, have_id);
