57434f62ee93e5ff0b3135f5837ea1550a57ab14
Jeremy Hager <jeremy.hager@disqo.com>
2023-02-10 16:02:58 -0800

    &&s and ||s

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
 fn main() {
-    // let are_equal_is_true = 1 == 1;
-    // let are_equal_is_false = 1 == 2;
-    // let are_not_equal = 1 != 2;
+    let have_boarding_pass = true;
+    let have_id = false;
+    let can_board  = have_boarding_pass && have_id;
 
-    let is_true = true;
-    let is_false = !is_true;
-
-    println!("is_true: {}, is_false: {}", is_true, is_false);
+    println!("Boarding Pass: {}, ID: {}", have_boarding_pass, have_id);
+    println!("Can board plane: {}", can_board);
 }
