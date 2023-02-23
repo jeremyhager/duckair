200036b7cbd3b510ab6c12e6a8206a6ce6424648
Jeremy Hager <jeremy.hager@disqo.com>
2023-02-22 21:33:36 -0800

    match part 1

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,8 @@
 fn main() {
-    let phrase = String::from("Duck Airlines");
-    let letter = phrase.chars().nth(15);
-
-    let value = match letter {
-        Some(character) => character.to_string(),
-        None => String::from("No value")
-    };
-
-    println!("{}", value)
+    let animal = "Duck";
+    match animal {
+        "Duck" => println!("Quack"),
+        "Dog" => println!("Bark"),
+        _ => println!("All quiet out here")
+    }
 }
