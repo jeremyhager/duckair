7f4848f102fcdcfc299d92e2e5f533987a57460e
Jeremy Hager <47301461+jeremyhager@users.noreply.github.com>
2023-03-03 14:03:19 -0800

    finished functions - scopes and ownerships is a little odd

--- src/side_lesson.rs
+++ src/side_lesson.rs
@@ -1,12 +1,19 @@
 pub fn side_lesson() {
-    let greater = return_greater(10, 5);
-    println!("{}", greater);
+    let mut original = String::from("original value");
+    println!("\nouter scope original:\t\"{}\"", original);
+    {
+        print_original(&original);
+        change_original(&mut original);
+        println!("inner scope original:\t\"{}\"", original);
+    }
 }
 
-fn return_greater(first: u8, second: u8) -> u8 {
-    if first > second {
-        first
-    } else {
-        second
-    }
+fn print_original(original: &String) {
+    println!("fn print_original:\t\"{}\"", original)
+}
+
+fn change_original(original: &mut String) {
+    let next = original;
+    *next = String::from("next value");
+    println!("fn change_original:\t\"{}\"", next);
 }
