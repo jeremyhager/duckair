deb91d2419d1772292eee6dd5bed11439ca2e989
Jeremy Hager <47301461+jeremyhager@users.noreply.github.com>
2023-03-03 13:10:11 -0800

    more borrowing and lifetimes

--- src/side_lesson.rs
+++ src/side_lesson.rs
@@ -1,9 +1,14 @@
 pub fn side_lesson() {
-    let referenced_int = 6;
-    let returned_value = return_one_parameter(&referenced_int);
-    println!("{}", returned_value);
+    let value_one = 24;
+    let value_two = 67;
+    let value = explicit_lifetime(&value_one, &value_two);
+    println!("{}", value);
 }
 
-fn return_one_parameter(value: &i32) -> &i32{
-    value
+fn explicit_lifetime<'a>(p1: &'a i32, p2: &'a i32) -> &'a i32{
+    if p1 > p2 {
+        p1
+    } else {
+        p2
+    }
 }
