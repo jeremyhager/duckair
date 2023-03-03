e81153642101a74ca182ca903d5a15207f72f94d
Jeremy Hager <47301461+jeremyhager@users.noreply.github.com>
2023-03-03 11:56:27 -0800

    Control flow (#4)
    * if/else intro
    
    * if/else and operators
    
    * finished out enums
    
    * finished option statement
    
    * match part 1
    
    * match statment part 2.1
    
    * finished match
    
    * finished match with enums
    
    * basic 10-count loop
    
    * skip number 5
    
    * finished while lesson
    
    * shuffled code around and module finished
    
    ---------
    
    Co-authored-by: Jeremy Hager <jeremy.hager@disqo.com>

---
+++ src/side_lesson.rs
@@ -0,0 +1,7 @@
+pub fn side_lesson() {
+    let duck_aircraft = ["boeing 737", "boeing 767", "boeing 787", "airbus 319", "airbus 320"];
+
+    for aircraft in duck_aircraft.iter() {
+        println!("{}", aircraft)
+    }
+}
