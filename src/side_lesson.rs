d83deef7ad2e30ffc5b5288c3309ab2307373682
Jeremy Hager <47301461+jeremyhager@users.noreply.github.com>
2023-03-03 16:33:21 -0800

    finished video

--- src/side_lesson.rs
+++ src/side_lesson.rs
@@ -1,61 +1,17 @@
-struct Boeing {
-    required_crew: u8,
-    range: u16
-}
-
-struct Airbus {
-    required_crew: u8,
-    range: u16
-}
-
-trait Flight {
-    fn is_legal(&self, required_crew: u8, availble_crew: u8, range: u16, distance: u16) -> bool;
-}
-
-impl Flight for Boeing {
-    fn is_legal(&self, required_crew: u8, availble_crew: u8, range: u16, distance: u16) -> bool {
-        if (availble_crew >= required_crew) && (range + 150 > distance) {
-            true
-        } else {
-            false
-        }
-    }
-}
-
-impl Flight for Airbus {
-    fn is_legal(&self, required_crew: u8, availble_crew: u8, range: u16, distance: u16) -> bool {
-        if (availble_crew >= required_crew) && (range + 280 > distance) {
-            true
-        } else {
-            false
-        }
-    }
-}
-
 pub fn side_lesson() {
-    let boeing = Boeing {
-        required_crew: 4,
-        range: 7370
-    };
+    let mut flights:Vec<&str> = Vec::new();
 
-    let airbus = Airbus {
-        required_crew: 7,
-        range: 5280
-    };
+    flights.push("DA113\tto Boston departs at 06:20");
+    flights.push("DA98\tto London departs at 09:43");
+    flights.push("DA428\tto Salt Lake City departs at 12:05");
+    flights.push("DA41\tto Berlin departs at 15:30");
+    flights.push("DA2815\tto Nashville departs at 17:11");
 
-    let boeing_is_legal = boeing.is_legal(
-        boeing.required_crew, 
-        18, 
-        boeing.range, 
-        2385,
-    );
+    flights.insert(2, "Some new option");
+    flights.remove(1);
 
-    let airbus_is_legal = airbus.is_legal(
-        airbus.required_crew, 
-        3, 
-        airbus.range, 
-        2200
-    );
+    for flight in flights.iter() {
+        println!("{}", flight);
+    }
 
-    println!("Is the boeing flight legal? {}\nIs the airbus flight legal? {}", boeing_is_legal, airbus_is_legal);
 }
