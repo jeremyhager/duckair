b642e61bda2415288dfae980bc8211b4af45b8db
Jeremy Hager <47301461+jeremyhager@users.noreply.github.com>
2023-03-05 15:39:16 -0800

    end video

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,11 @@
+use rand::Rng;
 use side_lesson::side_lesson;
-use geo::calculations::distance as distance_calc;
-
 mod side_lesson;
-mod geo;
 
 fn main() {
-    
-
-    let kcle_latitude_degrees: f64 = 41.4075;
-    let kcle_longitude_degrees: f64 = -81.851111;
-    let kslc_latitude_degrees: f64 = 40.7861;
-    let kslc_longitude_degrees: f64 = -111.9822;
-
-    let distance = distance_calc(kcle_latitude_degrees, kcle_longitude_degrees, kslc_latitude_degrees, kslc_longitude_degrees);
-
-    println!(
-        "The distance between the two points is {:.1} kilometers",
-        distance);
+    let mut rng = rand::thread_rng();
+    let random_num: f64 = rng.gen();
+    println!("{}", random_num);
 
     side_lesson();
 }
