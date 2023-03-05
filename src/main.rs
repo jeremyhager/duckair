e4880c10200127e494eebb720233d2d958010eba
Jeremy Hager <47301461+jeremyhager@users.noreply.github.com>
2023-03-05 15:44:36 -0800

    moved files around

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,9 @@
 use rand::Rng;
-use side_lesson::side_lesson;
-mod side_lesson;
 
 fn main() {
     let mut rng = rand::thread_rng();
     let random_num: f64 = rng.gen();
-    println!("{}", random_num);
 
-    side_lesson();
+    println!("{}", random_num);
 }
 
