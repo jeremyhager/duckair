40818f4008e3b504aaa6f45d97a732dad52be315
Jeremy Hager <47301461+jeremyhager@users.noreply.github.com>
2023-03-04 20:08:28 -0800

    finished sets

--- src/side_lesson.rs
+++ src/side_lesson.rs
@@ -1,21 +1,14 @@
-use std::collections::HashMap;
+use std::collections::HashSet;
 
 pub fn side_lesson() {
-    let mut flights = HashMap::new();
+    let mut flights = HashSet::new();
 
-    flights.insert("DA918", ("11:12", "Orlando"));
-    flights.insert("DA428", ("12:05", "Salt Lake City"));
-    flights.insert("DA98", ("09:43", "London"));
-    flights.insert("DA113", ("06:20", "Boston"));
-    flights.insert("DA41", ("15:30", "Berlin"));
-    flights.insert("DA2815", ("17:11", "Nashville"));
-
-    let flight_number = "DA531";
-    if !flights.contains_key(flight_number) {
-        flights.insert(flight_number, ("22:00", "Puerto Rico"));
-    } else {
-        println!("Flight {} is already entered", flight_number)
-    }
+    flights.insert(("DA918", "11:12", "Orlando"));
+    flights.insert(("DA428", "12:05", "Salt Lake City"));
+    flights.insert(("DA98", "09:43", "London"));
+    flights.insert(("DA113", "06:20", "Boston"));
+    flights.insert(("DA41", "15:30", "Berlin"));
+    flights.insert(("DA2815", "17:11", "Nashville"));
 
     for flight in flights.iter() {
         println!("{:?}", flight);
