7a5d8ffb876516d4cb466a7df44d990647e588f2
Jeremy Hager <47301461+jeremyhager@users.noreply.github.com>
2023-03-05 15:21:55 -0800

    starting point

--- src/main.rs
+++ src/main.rs
@@ -2,61 +2,30 @@ use side_lesson::side_lesson;
 
 mod side_lesson;
 
-struct Waypoint {
-    name: String,
-    latitude: f64,
-    longitude: f64
-}
-
-struct Segment {
-    start: Waypoint,
-    end: Waypoint
-}
-
-impl Segment {
-    fn new(start: Waypoint, end: Waypoint) -> Self {
-        Self {
-            start,
-            end
-        }
-    }
-
-    fn distance(&self) -> f64 {
-        const EARTH_RADIUS_IN_KILOMETERS: f64 = 6371.0;
-        let start_radians = self.start.latitude.to_radians();
-        let end_radians = self.end.latitude.to_radians();
-        let delta_latitude =
-            (self.start.latitude - self.end.latitude).to_radians();
-        let delta_longitude =
-            (self.start.longitude - self.end.longitude).to_radians();
-        let inner_central_angle = f64::powi((delta_latitude / 2.0).sin(), 2) + start_radians.cos() * end_radians.cos() * f64::powi((delta_longitude / 2.0).sin(), 2);
-        let central_angle = 2.0 * inner_central_angle.sqrt().asin();
-        let distance = EARTH_RADIUS_IN_KILOMETERS * central_angle;
-        distance
-    }
-}
-
 fn main() {
+    const EARTH_RADIUS_IN_KILOMETERS: f64 = 6371.0;
 
-    // const EARTH_RADIUS_IN_KM: f64 = 6371.0;
+    let kcle_latitude_degrees: f64 = 41.4075;
+    let kcle_longitude_degrees: f64 = -81.851111;
+    let kslc_latitude_degrees: f64 = 40.7861;
+    let kslc_longitude_degrees: f64 = -111.9822;
 
-    let kcle = Waypoint {
-        name: "KCLE".to_string(),
-        latitude: 41.4075,
-        longitude: -81.851111
-    };
+    let kcle_latitude_radians = kcle_latitude_degrees.to_radians();
+    let kslc_latitude_radians = kslc_latitude_degrees.to_radians();
 
-    let kslc = Waypoint {
-        name: "KSLC".to_string(),
-        latitude: 40.7861,
-        longitude: -111.9822
-    };
+    let delta_latitude = (kcle_latitude_degrees - kslc_latitude_degrees).to_radians();
+    let delta_longitude = (kcle_longitude_degrees - kslc_longitude_degrees).to_radians();
 
-    let kcle_kslc = Segment::new(kcle, kslc);
-    let distance = kcle_kslc.distance();
+    let inner_central_angle = f64::powi((delta_latitude / 2.0).sin(),2) +
+        kcle_latitude_radians.cos() * kslc_latitude_radians.cos() *
+            f64::powi((delta_longitude / 2.0).sin(),2);
+    let central_angle = 2.0 * inner_central_angle.sqrt().asin();
 
-    println!("distance:\t{:.1}", distance);
+    let distance = EARTH_RADIUS_IN_KILOMETERS * central_angle;
 
-    println!("\n\n[side lesson]:");
+    println!(
+        "The distance between the two points is {:.1} kilometers",
+        distance);
+
     side_lesson();
 }
