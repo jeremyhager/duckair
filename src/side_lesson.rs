d946e60d2ec7c93adbb1b91e204f87f43f8dbc4a
Jeremy Hager <47301461+jeremyhager@users.noreply.github.com>
2023-03-04 20:12:38 -0800

    Collections (#8)
    * finished video
    
    * finished VecDeque
    
    * finished lesson
    
    * finished sets

--- src/side_lesson.rs
+++ src/side_lesson.rs
@@ -1,61 +1,16 @@
-struct Boeing {
-    required_crew: u8,
-    range: u16
-}
-
-struct Airbus {
-    required_crew: u8,
-    range: u16
-}
-
-trait Flight {
-    fn is_legal(&self, required_crew: u8, availble_crew: u8, range: u16, distance: u16) -> bool;
-}
-
-impl Flight for Boeing {
-    fn is_legal(&self, required_crew: u8, availble_crew: u8, range: u16, distance: u16) -> bool {
-        if (availble_crew >= required_crew) && (range + 150 > distance) {
-            true
-        } else {
-            false
-        }
-    }
-}
-
-impl Flight for Airbus {
-    fn is_legal(&self, required_crew: u8, availble_crew: u8, range: u16, distance: u16) -> bool {
-        if (availble_crew >= required_crew) && (range + 280 > distance) {
-            true
-        } else {
-            false
-        }
-    }
-}
+use std::collections::HashSet;
 
 pub fn side_lesson() {
-    let boeing = Boeing {
-        required_crew: 4,
-        range: 7370
-    };
+    let mut flights = HashSet::new();
 
-    let airbus = Airbus {
-        required_crew: 7,
-        range: 5280
-    };
+    flights.insert(("DA918", "11:12", "Orlando"));
+    flights.insert(("DA428", "12:05", "Salt Lake City"));
+    flights.insert(("DA98", "09:43", "London"));
+    flights.insert(("DA113", "06:20", "Boston"));
+    flights.insert(("DA41", "15:30", "Berlin"));
+    flights.insert(("DA2815", "17:11", "Nashville"));
 
-    let boeing_is_legal = boeing.is_legal(
-        boeing.required_crew, 
-        18, 
-        boeing.range, 
-        2385,
-    );
-
-    let airbus_is_legal = airbus.is_legal(
-        airbus.required_crew, 
-        3, 
-        airbus.range, 
-        2200
-    );
-
-    println!("Is the boeing flight legal? {}\nIs the airbus flight legal? {}", boeing_is_legal, airbus_is_legal);
+    for flight in flights.iter() {
+        println!("{:?}", flight);
+    }
 }
