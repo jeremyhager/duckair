fd3842a81ec32f40665c48d51882e7a401a0b41b
Jeremy Hager <47301461+jeremyhager@users.noreply.github.com>
2023-03-04 22:09:49 -0800

    Concurrency (#10)
    * creating threads
    
    * finished small amount of thread

--- src/side_lesson.rs
+++ src/side_lesson.rs
@@ -1,12 +1,39 @@
-use std::ops::Add;
+use std::thread;
+use std::sync::mpsc;
+use std::sync::mpsc::{Receiver, Sender};
 
 pub fn side_lesson() {
-    let sum = add(256, 262);
-    println!("{}", sum);
+    let (john_tx, john_rx) = mpsc::channel();
+    let (sarah_tx, sarah_rx) = mpsc::channel();
+
+    let john_handle = thread::spawn(move || {
+        john_chat(sarah_tx, john_rx)
+    });
+    let sarah_handle = thread::spawn(move || {
+        sarah_chat(john_tx, sarah_rx)
+    });
+
+    match john_handle.join() {
+        Ok(_) => {}
+        Err(_) => {}
+    }
+
+    match sarah_handle.join() {
+        Ok(_) => {}
+        Err(_) => {}
+    }
 }
 
-fn add<T>(operand1: T, operand2: T) -> T
-where T: Add<Output =T>
-{
-    operand1 + operand2
+fn sarah_chat(john_tx:Sender<&str>, sarah_rx: Receiver<&str>) {
+    let result = sarah_rx.recv();
+    println!("{}", result.unwrap());
+
+    let _send_result = john_tx.send("Hello John");
+}
+
+fn john_chat(sarah_tx:Sender <&str>, john_rx:Receiver<&str>) {
+    let _send_result = sarah_tx.send("Hello Sarah");
+    let result = john_rx.recv();
+
+    println!("{}", result.unwrap());
 }
